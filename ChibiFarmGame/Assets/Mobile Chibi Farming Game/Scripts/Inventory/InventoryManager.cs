ca1bedbd8e3efe35ec432aeeffdf9cd65b88322e
busrasimsek <busrasimsek929@gmail.com>
2023-12-29 19:26:56 +0300

    saving and loading our inventory

--- ChibiFarmGame/Assets/Mobile Chibi Farming Game/Scripts/Inventory/InventoryManager.cs
+++ ChibiFarmGame/Assets/Mobile Chibi Farming Game/Scripts/Inventory/InventoryManager.cs
@@ -1,16 +1,18 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class InventoryManager : MonoBehaviour
 {
     private Inventory inventory;
+    private string dataPath;
     
-    // Start is called before the first frame update
     void Start()
     {
-        inventory = new Inventory();
+        //inventory = new Inventory();
+        dataPath = Application.dataPath + "/inventoryData.txt";
+        LoadInventory();
 
         CropTile.onCropHarvested += CropHarvestedCallback;
     }
@@ -18,13 +20,34 @@ public class InventoryManager : MonoBehaviour
     private void OnDestroy()
     {
         CropTile.onCropHarvested -= CropHarvestedCallback;
-        
     }
 
     private void CropHarvestedCallback(CropType cropType)
     {
         inventory.CropHarvestedCallback(cropType);
+        SaveInventory();
+    }
+
+    private void LoadInventory()
+    {
+        string data = "";
+
+        if (File.Exists(dataPath) == false)
+            File.WriteAllText(dataPath, "{}");  // if  the file does not exist, create it and write an empty JSON in it
         
+        
+        data = File.ReadAllText(dataPath);
+        inventory = JsonUtility.FromJson<Inventory>(data);
+
+        if (inventory == null)
+            inventory = new Inventory();
+    }
+
+
+    private void SaveInventory()
+    {
+        string data = JsonUtility.ToJson(inventory, true);
+        File.WriteAllText(dataPath, data);
     }
 
 }
