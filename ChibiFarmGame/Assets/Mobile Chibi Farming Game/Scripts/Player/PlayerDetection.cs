e8239691357382c6cb09fc96271ba4c4f442af8e
busrasimsek <busrasimsek929@gmail.com>
2024-03-05 12:50:09 +0300

    tree detection

--- ChibiFarmGame/Assets/Mobile Chibi Farming Game/Scripts/Player/PlayerDetection.cs
+++ ChibiFarmGame/Assets/Mobile Chibi Farming Game/Scripts/Player/PlayerDetection.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class PlayerDetection : MonoBehaviour
@@ -9,5 +10,27 @@ public class PlayerDetection : MonoBehaviour
          Chunk chunk = other.GetComponentInParent<Chunk>();
          chunk.TryUnlock();
       }
+   }
+
+   private void OnTriggerEnter(Collider other)
+   {
+      if (other.TryGetComponent(out AppleTree tree))
+         TriggeredAppleTree(tree);
+   }
+
+   private void OnTriggerExit(Collider other)
+   {
+      if (other.TryGetComponent(out AppleTree tree))
+         ExitedAppleTreeZone(tree);
+   }
+
+   private void TriggeredAppleTree(AppleTree tree)
+   {
+      Debug.Log(" We have entered a tree zone ");
+   }
+
+   private void ExitedAppleTreeZone(AppleTree tree)
+   {
+      Debug.Log(" We have exited a tree zone ");
    }
 }
